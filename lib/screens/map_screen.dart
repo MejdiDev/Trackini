c8352455513ccabd62f65cb27f7b973d436cdcef
Mejdi Chenoufi <mejdchennoufi06@gmail.com>
2022-04-27 19:45:21 +0300

    Map Function Commit

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -16,24 +17,10 @@ class MapScreenState extends State<MapScreen> {
   final ref = FirebaseDatabase.instance.ref('/latLng');
 
   late Marker marker;
-  late CameraPosition camPos;
+  CameraPosition? camPos;
   double speed = 0.0;
-  var timestamp;
-
-  // CameraPosition camPos = CameraPosition(
-  // target: LatLng(34.64879975941712, 10.590110805813373),
-  // zoom: 17.5,
-  // );
-  //
-  // Marker marker = Marker(
-  // markerId: const MarkerId('Car Location'),
-  // position: LatLng(34.64879975941712, 10.590110805813373),
-  //
-  // infoWindow: InfoWindow(
-  // title: 'Speed',
-  // snippet: '0.0 Km/h'
-  // )
-  // );
+  DateTime? timestamp;
+  bool widowVis = false;
 
   var carPos;   //The variable in which the location data coming from the database is stored.
   void fetchData() async {
@@ -42,91 +29,133 @@ class MapScreenState extends State<MapScreen> {
       "assets/images/car.png",
     );
 
-    ref.onValue.listen((event) {
-      final location = event.snapshot.value;
-      var lastCarPos = carPos;
+
+    DatabaseEvent event = await ref.once();
+    final location = event.snapshot.value;
+    var lastCarPos = carPos;
+
+    if(location.toString() == lastCarPos) {
+      if(speed == 0.0) return;
 
       setState(() {
-        carPos = location;
-        LatLng newPos = LatLng(parsecords(carPos)[0], parsecords(carPos)[1]);
-
-        //Update marker and camera position to the new position we got in the database.
-        camPos = CameraPosition(
-          target: newPos,
-          zoom: 17.5,
-        );
-
-        marker = Marker(
-          markerId: const MarkerId('Car Location'),
-          position: newPos, //position of the marker.
-          icon: markerbitmap, //Icon for Marker.
-        );
+        speed = 0.0;
       });
+      return;
+    }
 
-      double calculateDistance(lat1, lon1, lat2, lon2){
-        var p = 0.017453292519943295;
-        var a = 0.5 - cos((lat2 - lat1) * p)/2 +
-            cos(lat1 * p) * cos(lat2 * p) *
-                (1 - cos((lon2 - lon1) * p))/2;
+    setState(() {
+      carPos = location;
+      LatLng newPos = LatLng(parsecords(carPos)[0], parsecords(carPos)[1]);
 
-        return 12742 * asin(sqrt(a));
-      }
-
-      var lastTimestamp = timestamp;
-      timestamp = DateTime.now();
-
-      var timeDiff = timestamp.difference(lastTimestamp).inSeconds;   //The time between positions A and B in seconds.
-      var distanceDiff = calculateDistance(
-          parsecords(lastCarPos)[0],
-          parsecords(lastCarPos)[1],
-          parsecords(carPos)[0],
-          parsecords(carPos)[1]
+      //Update marker and camera position to the new position we got in the database.
+      camPos = CameraPosition(
+        target: newPos,
+        zoom: 17.5,
       );
 
-      speed = distanceDiff / (timeDiff / 3600);
-      transition();
+      marker = Marker(
+        markerId: const MarkerId('Car Location'),
+        position: newPos, //position of the marker.
+        icon: markerbitmap, //Icon for Marker.
+        consumeTapEvents: true,
+
+        onTap: () {
+          setState(() {
+            widowVis = true;
+          });
+        }
+      );
     });
+
+    var lastTimestamp = timestamp;
+    timestamp = DateTime.now();
+
+    var timeDiff = timestamp!.difference(lastTimestamp!).inSeconds;   //The time between positions A and B in seconds.
+    var distanceDiff = calculateDistance(
+        parsecords(lastCarPos)[0],
+        parsecords(lastCarPos)[1],
+        parsecords(carPos)[0],
+        parsecords(carPos)[1]
+    );
+
+    speed = distanceDiff / (timeDiff / 3600);
+    transition();
   }
 
   @override
   void initState() {
-    fetchData();
+    Future.delayed(const Duration(seconds: 2), () {
+      Timer.periodic(const Duration(milliseconds: 500), (Timer timer) async {
+        fetchData();
+      });
+    });
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body:
+      (camPos == null) ?
+      const SpinKitDualRing(
+        color: Colors.blueAccent,
+        size: 80.0,
+        lineWidth: 10.0,
+      ):
+
+      Stack(
+        alignment: Alignment.centerRight,
         children: [
           GoogleMap(
             mapType: MapType.normal,
-            initialCameraPosition: camPos,
+            initialCameraPosition: camPos!,
             onMapCreated: (GoogleMapController controller) {
               _controller.complete(controller);
             },
 
+            onTap: (pos) => setState(() {
+              widowVis = false;
+            }),
+
             markers: {marker},
           ),
 
-          Positioned(
-            bottom: 0.0,
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 200),
+            bottom: widowVis ? 10.0 : -100.0,
             left: 0.0,
             child: Container(
-              width: 100.0,
-              height: 70.0,
-              alignment: Alignment.center,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(15.0)
-                ),
+              width: (MediaQuery.of(context).size.width - 20.0),
+              margin: const EdgeInsets.only(left: 10.0),
+              height: 75.0,
+              padding: const EdgeInsets.only(left: 25.0),
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
                 color: Colors.white,
+
+                borderRadius: const BorderRadius.all(
+                    Radius.circular(15.0)
+                ),
+
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1.0,
+                ),
+
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.8),
+                    spreadRadius: 5,
+                    blurRadius: 8,
+                    offset: const Offset(0, 8),
+                  ),
+                ],
               ),
               child: Text(
-                'Speed: \n${speed.toStringAsFixed(1)} Km/h',
-                textAlign: TextAlign.center,
+                'Speed:  ${speed.toStringAsFixed(1)} Km/h',
                 style: const TextStyle(
-                  fontSize: 15.5
+                  fontSize: 18.0,
                 ),
               ),
             ),
@@ -138,7 +167,16 @@ class MapScreenState extends State<MapScreen> {
 
   void transition() async {
     final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(camPos));
+    controller.animateCamera(CameraUpdate.newCameraPosition(camPos!));
+  }
+
+  double calculateDistance(lat1, lon1, lat2, lon2){
+    var p = 0.017453292519943295;
+    var a = 0.5 - cos((lat2 - lat1) * p)/2 +
+        cos(lat1 * p) * cos(lat2 * p) *
+            (1 - cos((lon2 - lon1) * p))/2;
+
+    return 12742 * asin(sqrt(a));
   }
 
   List parsecords(String coords) {
